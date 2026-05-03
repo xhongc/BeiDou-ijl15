938e72568af2f7776eecafd8dae1a4db645a6ac8
charlesxie <408737515@qq.com>
2026-05-03 11:58:48 +0800

    1

--- ezorsia/dllmain.cpp
+++ ezorsia/dllmain.cpp
@@ -124,7 +124,9 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
 		Hook_StringPool__GetString(true); //hook stringpool modification //ty !! popcorn //ty darter
 		Hook_lpfn_NextLevel(true);
 		HookSaveGlobal(true);
-		HookHpMpAlertRecv(true);
+		// Disabled for now: this receive hook is the most likely source of the
+		// immediate crash during character entry / field load.
+		// HookHpMpAlertRecv(true);
 		//Hook_get_unknown(true);
 		//Hook_get_resource_object(true); //helper function hooks  //ty teto for helping me get started
 		//Hook_com_ptr_t_IWzProperty__ctor(true);
